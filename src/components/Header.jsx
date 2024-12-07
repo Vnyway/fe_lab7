28d8976be819521d6977b46abf8d06142c22d71c
Vnywvy <bohdan05zbr@gmail.com>
2024-12-07 12:14:20 +0200

    Task 2: Goods data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { el1Click } from "../functions/task1";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -7,6 +8,7 @@ const Header = () => {
       <h1 id="el1" onClick={() => el1Click()}>
         Житченко Богдан Русланович
       </h1>
+      <Link to="/task2">Завдання 2</Link>
     </header>
   );
 };
---
+++ src/constants/index.js
@@ -0,0 +1,43 @@
+export const goods = [
+  {
+    id: 1,
+    image:
+      "https://www.lori.com.ua/wp-content/uploads/2020/11/Oslo-140-natural-side1.jpg",
+    title: "Стіл Дефолтний",
+    price: 500,
+  },
+  {
+    id: 2,
+    image:
+      "https://cidien.lazurit.com/images/unsafe/fit-in/1000x1000/upload.lazurit.com/upload/iblock/2cb/unl9rn0sudlpxrryfwq3efaz0kiptzil/mm0070_Divan_krovat_uglovoy_Kil_3_maxx_01-965.jpg",
+    title: "Диван Норм",
+    price: 500,
+  },
+  {
+    id: 3,
+    image:
+      "https://vetromebel.com/storage/17329/66b4b94a5245a_654909d8842f7_m-70-siriy-chorniy3.jpg",
+    title: "Стілець Модний",
+    price: 500,
+  },
+  {
+    id: 4,
+    image: "https://polki.com.ua/wp-content/uploads/2015/12/polka8.jpg",
+    title: "Полиця",
+    price: 500,
+  },
+  {
+    id: 5,
+    image:
+      "https://www.easyloft.net/wp-content/uploads/2022/04/d57ee460b4fc7302ab02e0d0bcli-dlya-doma-i-interera-ulichnaya-lavka.jpg",
+    title: "Лавка",
+    price: 500,
+  },
+  {
+    id: 6,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD2eBho_suwBm9W8fz-xhqv3IGxIPCq13p_w&s",
+    title: "Комод",
+    price: 500,
+  },
+];
