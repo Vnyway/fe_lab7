3433b45611933b766c597de781d8561bb1676156
Vnywvy <bohdan05zbr@gmail.com>
2024-12-07 12:26:35 +0200

    Predeploy changes 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Task1, Task2 } from "./pages";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Task1 />} />
         <Route path="/task2" element={<Task2 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
